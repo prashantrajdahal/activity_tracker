de79233bc5f9cf0f20f7941cd1669855631704eb
prashantrajdahal <workwithprashantdahal@gmail.com>
2026-01-22 02:17:13 +0545

    config:add centralized configuration

---
+++ src/config.py
@@ -0,0 +1,10 @@
+DB_CONFIG = {
+    "DB_CONNECTION": "mysql",
+    "DB_HOST": "127.0.0.1",
+    "DB_PORT": 3306,
+    "DB_DATABASE": "my_website",
+    "DB_USERNAME": "root",
+    "DB_PASSWORD": ""
+}
+
+LOG_FILE="activity_tracker.log"
